3feb8ef75a37eab53d80ba588dfa89249fe9b41c
Reversedeer <ysjvillmark@gmail.com>
2024-11-26 23:02:21 +0800

    ✨Update nonebot_plugin_game

--- nonebot_plugin_game/utils.py
+++ nonebot_plugin_game/utils.py
@@ -1,5 +1,6 @@
 """配置文件"""
 
+
 class Utils:
     def __init__(self) -> None:
         """用法"""
@@ -8,13 +9,7 @@ class Utils:
         指令2：钓鱼
         """
 
-        self.not_allow = '还未开启游戏功能，请管理|群主|超级用户发送：开启/关闭游戏功能'
-        
-
-
+        self.not_allow = "还未开启游戏功能，请管理|群主|超级用户发送：开启/关闭游戏功能"
 
 
 utils = Utils()
-
-
-
